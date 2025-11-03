2ca1f6aa197653705014d4b2ee9d7e40c44b2e74
pepper-sister <yuyu7123@naver.com>
2025-11-04 04:18:11 +0900

    feat: 푸터 추가

---
+++ src/components/Footer.js
@@ -0,0 +1,10 @@
+import "../styles/Footer.css";
+
+export default function Footer() {
+  return (
+    <div className="footer">
+      <p className="footer__txt">© 2024. Busan's OndeGande All rights reserved.</p>
+      <p className="footer__txt">Contact: yuyu7123@naver.com</p>
+    </div>
+  );
+}
