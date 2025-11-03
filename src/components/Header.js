a236fcf51e11271a28705f70513390d775d8ec39
pepper-sister <yuyu7123@naver.com>
2025-11-04 04:07:26 +0900

    feat: 헤더(navbar) 추가

---
+++ src/components/Header.js
@@ -0,0 +1,17 @@
+import logo from "../assets/logo.png";
+import "../styles/Header.css";
+
+export default function Header() {
+  return (
+    <div className="header">
+      <img className="header__img" src={logo} alt="logo" />
+
+      <div className="header__right">
+        <button className="header__right__tap">코스 짜보이소</button>
+        <button className="header__right__tap">인자 머하노?</button>
+        <button className="header__right__tap">이래 가보이소</button>
+        <button className="header__right__tap">유튜바 코스</button>
+      </div>
+    </div>
+  );
+}
