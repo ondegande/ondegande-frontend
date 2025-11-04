77e7b10f50989d06561b68e6ec6d535834ef362c
pepper-sister <yuyu7123@naver.com>
2025-11-04 13:54:32 +0900

    feat: 라우터 이동시 맨 위로 이동 추가

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import { Outlet } from "react-router-dom";
+import "./App.css";
+
 import Header from "./components/Header";
 import Footer from "./components/Footer";
-import "./App.css";
+import ScrollToTop from "./components/ScrollToTop";
 
 function App() {
   return (
     <div className="App">
+      <ScrollToTop />
       <Header />
       <main>
         <Outlet />
