ab8944d4dd7de5feb141aab5262067aede9d48ce
pepper-sister <yuyu7123@naver.com>
2025-11-04 13:55:14 +0900

    chore: 코스 짜보이소 라우터 추가

--- src/home/components/Feature.js
+++ src/home/components/Feature.js
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faPenToSquare, faCompass, faMap, faSquareCaretRight } from "@fortawesome/free-regular-svg-icons";
 import "../styles/feature.css";
@@ -8,14 +9,16 @@ export default function Feature() {
       <h1 className="f-36 w-600">부산 여행 플래닝</h1>
       <h2 className="f-18 w-400 c-555555">당신의 여행 스타일에 맞춘 완벽한 가이드</h2>
       <div className="features grid-2 gap-28">
-        <div className="feature column j-space-between gap-40 br-12 cursor">
-          <div className="feature__icons br-12 bg-0096ff">
-            <FontAwesomeIcon className="feature__icon" icon={faPenToSquare} />
+        <Link to="/makecourse">
+          <div className="feature column j-space-between gap-40 br-12 cursor">
+            <div className="feature__icons br-12 bg-0096ff">
+              <FontAwesomeIcon className="feature__icon" icon={faPenToSquare} />
+            </div>
+            <h3 className="f-24 w-600">코스 짜보이소</h3>
+            <p className="w-400 c-555555">나만의 여행 코스를 직접 만들고 공유</p>
+            <div className="feature__button c-155dfc">자세히 보기 →</div>
           </div>
-          <h3 className="f-24 w-600">코스 짜보이소</h3>
-          <p className="w-400 c-555555">나만의 여행 코스를 직접 만들고 공유</p>
-          <div className="feature__button c-155dfc">자세히 보기 →</div>
-        </div>
+        </Link>
         <div className="feature column j-space-between gap-40 br-12 cursor">
           <div className="feature__icons br-12 bg-ffa200">
             <FontAwesomeIcon className="feature__icon" icon={faCompass} />
--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
-import ReactDOM from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import App from "./App";
-import Home from "./pages/Home";
+import ReactDOM from "react-dom/client";
 import "./index.css";
 
-const pages = [{ path: "/", element: <Home /> }];
+import App from "./App";
+import Home from "./pages/Home";
+import MakeCourse from "./pages/MakeCourse";
+
+const pages = [
+  { path: "/", element: <Home /> },
+  { path: "/makecourse", element: <MakeCourse /> },
+];
 const router = createBrowserRouter([
   {
     path: "/",
