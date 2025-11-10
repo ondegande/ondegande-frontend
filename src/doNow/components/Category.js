4e281d73e9bc07a2c933c1509818edc399d21f6f
pepper-sister <yuyu7123@naver.com>
2025-11-11 06:12:13 +0900

    style: css 클래스 추가

--- src/doNow/components/Category.js
+++ src/doNow/components/Category.js
@@ -2,23 +2,25 @@ import "../styles/category.css";
 
 export default function Category({ category, handleCategoryClick }) {
   return (
-    <div className="column gap-20">
+    <div className="category__section column gap-20 width-50">
       <h1 className="f-18">카테고리</h1>
       <div className="row gap-10">
         <button
-          className={`category__button br-12 f-14 w-400 cursor ${category === "sightseeing" ? "active" : ""}`}
+          className={`category__button width-100 br-12 f-14 w-400 cursor ${category === "sightseeing" ? "active" : ""}`}
           onClick={() => handleCategoryClick("sightseeing")}
         >
           🏛️ 관광지
         </button>
         <button
-          className={`category__button br-12 f-14 w-400 cursor ${category === "food" ? "active" : ""}`}
+          className={`category__button width-100 br-12 f-14 w-400 cursor ${category === "food" ? "active" : ""}`}
           onClick={() => handleCategoryClick("food")}
         >
           🍽️ 맛집
         </button>
         <button
-          className={`category__button br-12 f-14 w-400 cursor ${category === "accommodation" ? "active" : ""}`}
+          className={`category__button width-100 br-12 f-14 w-400 cursor ${
+            category === "accommodation" ? "active" : ""
+          }`}
           onClick={() => handleCategoryClick("accommodation")}
         >
           🏨 숙소
