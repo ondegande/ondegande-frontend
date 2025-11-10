1065ac7a4faf4f50fad9a77f755e659b80570adf
pepper-sister <yuyu7123@naver.com>
2025-11-11 06:11:33 +0900

    style: css 구조 수정

--- src/makeCourse/components/PlansManager.js
+++ src/makeCourse/components/PlansManager.js
@@ -18,27 +18,29 @@ export default function PlansManager({ days, addDay, clipboardBtnRef, toggleMapV
   );
 
   return (
-    <div className="plan__buttons column gap-10 bg-ffffff br-12">
+    <div className="plan__buttons column gap-16 bg-ffffff br-12">
       <h3 className="f-18">일정 관리</h3>
-      <button className="buttons__day row j-center a-center bg-155dfc br-12 c-ffffff cursor" onClick={addDay}>
-        <FontAwesomeIcon className="day__icon c-000000" icon={faCalendarPlus} />
-        Day 추가
-      </button>
-      <button
-        className="buttons__clip row j-center a-center c-155dfc bg-ffffff br-12 f-14 w-400 cursor"
-        ref={clipboardBtnRef}
-        data-clipboard-text={generateShareText()}
-      >
-        <FontAwesomeIcon className="clip__icon" icon={faCopy} />
-        복사
-      </button>
-      <button
-        className="buttons__move row j-center a-center bg-f9f8f8 br-12 f-14 w-400 cursor"
-        onClick={toggleMapVisibility}
-      >
-        <FontAwesomeIcon className="move__icon" icon={isMapVisible ? faEyeSlash : faEye} />
-        {isMapVisible ? "동선 숨기기" : "동선 확인"}
-      </button>
+      <div className="plan__buttons__section column gap-10">
+        <button className="buttons__day row j-center a-center bg-155dfc br-12 c-ffffff cursor" onClick={addDay}>
+          <FontAwesomeIcon className="day__icon c-000000" icon={faCalendarPlus} />
+          Day 추가
+        </button>
+        <button
+          className="buttons__clip row j-center a-center c-155dfc bg-ffffff br-12 f-14 w-400 cursor"
+          ref={clipboardBtnRef}
+          data-clipboard-text={generateShareText()}
+        >
+          <FontAwesomeIcon className="clip__icon" icon={faCopy} />
+          복사
+        </button>
+        <button
+          className="buttons__move row j-center a-center bg-f9f8f8 br-12 f-14 w-400 cursor"
+          onClick={toggleMapVisibility}
+        >
+          <FontAwesomeIcon className="move__icon" icon={isMapVisible ? faEyeSlash : faEye} />
+          {isMapVisible ? "동선 숨기기" : "동선 확인"}
+        </button>
+      </div>
     </div>
   );
 }
